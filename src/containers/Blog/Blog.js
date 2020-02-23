e09eb1b52963eff722b1007ffeac2db6b1be79d3
blaec <odeskonst@yandex.ru>
2020-02-23 22:39:53 +0200

    200. Handling Errors Locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 
 import Post from '../../components/Post/Post';
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     };
 
     componentDidMount() {
@@ -24,6 +25,10 @@ class Blog extends Component {
                 });
                 this.setState({posts: updatedPosts})
                 // console.log(response);
+            })
+            .catch(error => {
+                // console.log(error);
+                this.setState({error: true})
             });
     }
 
@@ -31,13 +36,16 @@ class Blog extends Component {
         this.setState({selectedPostId: id})
     };
 
-    render () {
-        const posts = this.state.posts.map(post => {
-            return <Post key={post.id}
-                         title={post.title}
-                         author={post.author}
-                         clicked={() => this.postSelectedHandler(post.id)}/>
-        });
+    render() {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post key={post.id}
+                             title={post.title}
+                             author={post.author}
+                             clicked={() => this.postSelectedHandler(post.id)}/>
+            });
+        }
         return (
             <div>
                 <section className="Posts">
@@ -47,7 +55,7 @@ class Blog extends Component {
                     <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
-                    <NewPost />
+                    <NewPost/>
                 </section>
             </div>
         );
