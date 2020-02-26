b318de4104ca9ad420cdcabe8b7926af7990f644
blaec <odeskonst@yandex.ru>
2020-02-26 21:14:25 +0200

    231. Styling the Active Route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 // import axios from 'axios';
-import {Route, Link} from 'react-router-dom';
+import {Route, NavLink} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from "./Posts/Posts";
@@ -13,12 +13,22 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li>
+                                <NavLink to="/"
+                                         exact
+                                         activeClassName="my-active"
+                                         activeStyle={{
+                                             color: '#fa923f',
+                                             textDecoration: 'underline'
+                                         }}>
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li><NavLink to={{
                                 pathname: "/new-post",
                                 hash: '@submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
@@ -27,8 +37,8 @@ class Blog extends Component {
                 {/*       render={() => <h1>Home</h1>}/>*/}
                 {/*<Route path="/"*/}
                 {/*       render={() => <h1>Home 2</h1>}/>*/}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" component={NewPost}/>
             </div>
         );
     }
